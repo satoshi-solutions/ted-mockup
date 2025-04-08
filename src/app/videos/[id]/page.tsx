7e7853375467e9180f85749df6b044b1ba5f1521
satoshi-solutions <satoshi-bitcoin.solutions@gmail.com>
2025-04-08 03:44:37 -0400

    third

--- src/app/videos/[id]/page.tsx
+++ src/app/videos/[id]/page.tsx
@@ -49,7 +49,7 @@ export default async function VideoPage({ params }: VideoPageProps) {
         </div>
         
         <div className="lg:col-span-1">
-          <RelatedVideos videos={relatedVideos} />
+          <RelatedVideos videos={relatedVideos} currentVideoId={params.id} />
         </div>
       </div>
     </div>
